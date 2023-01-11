b05f1a4107c6303856a995fb958c8cc33f8a6e15
Prajwal <82059926+sprajwal11@users.noreply.github.com>
2023-01-11 10:25:22 +0530

    bug fixed

--- Event_Organization.sol
+++ Event_Organization.sol
@@ -16,7 +16,7 @@ contract Event_Organization{
     mapping(address=>mapping(uint=>uint)) public tickets;
     uint public nextId;
     
-    function createEvent (string memory name, uint date, uint price, uint,uint ticketCount) external {
+    function createEvent (string memory name, uint date, uint price,uint ticketCount) external {
         require(date>block.timestamp, "You can organize the event for the future date only");
         require(ticketCount>0,"Ticket size can't be zero");
 
